5e03f89a7d00cba0d9a633a5fcdff4c69b579993
chiara2201 <71043804+chiara2201@users.noreply.github.com>
2023-10-25 11:07:46 +0100

    adds articleDetailsPage

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 export const Article = ({ article }) => {
 	return (
 		<>
-			<h2>{article.title}</h2>
-			<div>
-				<div>{article.author}</div>
-				<div>{article.created_at}</div>
-				<div>👍: {article.votes}</div>
+			<div className="single-article-container">
+				<h2>{article.title}</h2>
+				<div>
+					<div>{article.author}</div>
+					<div>Posted on: {article.created_at}</div>
+					<div>👍: {article.votes}</div>
+				</div>
+				<img src={article.article_img_url} alt="{article.title}" />
+				<p>{article.body}</p>
 			</div>
-			<img src={article.article_img_url} alt="{article.title}" />
-			<p>{article.body}</p>
 		</>
 	)
 }
