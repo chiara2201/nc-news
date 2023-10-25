bd90765721bf3a9270b84186b76be8d573ebe806
chiara2201 <71043804+chiara2201@users.noreply.github.com>
2023-10-25 16:22:03 +0100

    adds Voter

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Voter } from './Voter'
 
 export const Article = ({ article }) => {
 	const date = article.created_at.replace('T', ' ').replace('Z', '')
@@ -10,10 +11,11 @@ export const Article = ({ article }) => {
 				<div>
 					<div>{article.author}</div>
 					<div>Posted on: {date}</div>
-					<div>👍: {article.votes}</div>
 				</div>
 				<img src={article.article_img_url} alt="{article.title}" />
 				<p>{article.body}</p>
+				<div>👍: {article.votes}</div>
+				<Voter votes={article.votes} />
 			</div>
 		</>
 	)
